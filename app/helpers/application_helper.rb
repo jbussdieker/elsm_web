a96ca3b2df7c7892a0a6d42bbafae1e507b0bdbf
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-09-17 02:41:01 -0700

    Badges and stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def generic_badge(msg)
+    if msg.to_s.downcase == "running"
+      content_tag(:i, msg, class: "badge badge-success")
+    elsif msg.to_s.downcase == "changed"
+      content_tag(:i, msg, class: "badge badge-warning")
+    else
+      content_tag(:i, msg, class: "badge")
+    end
+  end
 end
